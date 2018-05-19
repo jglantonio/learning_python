3a7cad5aa078cb05109e5d1c40dd6bcd393c83cd
joseantoniogl <joseantoniogonzalezlopez@gmail.com>
2018-05-19 17:01:34 +0200

    Aprendizaje sobre conversiones

---
+++ curso001/04_conversiones_operadores.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Strings y funciones para trabajar con ellos
+
+variable = "Tengo un conejo ";
+print(variable);
+variable2 = "llamado POL";
+print(variable2);
+print(variable + variable2);
+print(variable*2);
+
+# function len
+
+length_variable = len(variable);
+print("La variable "+variable+", tiene ",length_variable);
+
+# primera letra del string
+
+print(variable[0]);
+
