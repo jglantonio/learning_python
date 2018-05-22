d42c22f986bb780c610a68090a6d3461a330515f
joseantoniogl <joseantoniogonzalezlopez@gmail.com>
2018-05-22 22:58:56 +0200

    Aprendiendo sobre truplas y sets

---
+++ curso001/06_tuplas.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Dimensiones de una caja
+
+dimension = 3,4,5;
+
+longitud , alto , ancho = dimension;
+
+print("La dimension es de {}x{}x{}cm".format(longitud,alto,ancho))
